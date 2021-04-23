31217c0eb9fa45bb443b6fa3afbfc04f097a822a
Mahsum Kizmaz <mahsumkizmaz@gmail.com>
2021-04-23 14:07:10 +0200

    ajout de toutes les fonctions

---
+++ perimeter.py
@@ -0,0 +1,12 @@
+import math
+
+def RectanglePerimeter(large, longueur):
+	return 2*large+2*longueur
+
+def CirclePerimeter(ray):
+	return 2*math.pi*ray
+
+def SquarePerimeter (large):
+	return large*4
+def TrianglePerimeter(large):
+	return large*3
