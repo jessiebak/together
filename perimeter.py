60a7ccb4b836549a00e218646d1f7c164ec84295
Mahsum Kizmaz <mahsumkizmaz@gmail.com>
2021-04-23 14:42:42 +0200

    ajout de la fonction square

--- perimeter.py
+++ perimeter.py
@@ -6,5 +6,7 @@ def RectanglePerimeter(large, longueur):
 def CirclePerimeter(ray):
 	return 2*math.pi*ray
 
+def SquarePerimeter(large):
+	return large*4
 def TrianglePerimeter(large1, large2, large3):
 	return large1+large2+large3
