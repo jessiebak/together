422f0793b6923a19f25ec4886a3be9d41492a841
Eleonor Sana <eleonorsana97@gmail.com>
2021-04-23 14:12:42 +0200

    Creation du fichier area.py

---
+++ area.py
@@ -0,0 +1,17 @@
+import math
+
+def RectangleArea(_base, _hauteur):
+    sum=_base*_hauteur
+    return sum
+
+def CircleArea(_ray):
+    sum=math.pi*_ray*_ray
+    return sum
+
+def SquareArea(_base, _hauteur):
+    sum=_base*_hauteur
+    return sum
+
+def TriangleArea(_base, _hauteur):
+    sum=(_base*_hauteur)/2
+    return sum
