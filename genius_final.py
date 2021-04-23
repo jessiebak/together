8de7412af22a837efeb4ef45f6de89f6868f3f65
Jessie Bakashika <jessie.bakashika@gmail.com>
2021-04-23 13:40:31 +0200

    - genius clean

---
+++ genius_final.py
@@ -0,0 +1,15 @@
+def Add(_number1, _number2):
+    return _number1 + _number2
+
+
+def Substract(_number1, _number2):
+    return _number1 - _number2
+
+
+def Multiply(_number1, _number2):
+    return _number1 * _number2
+
+
+def Divide(_number1, _number2):
+    return _number1 / _number2
+
