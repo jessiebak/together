c01d7298b10a496cd4089926ba9af6529e3f9d81
Jessie Bakashika <jessie.bakashika@gmail.com>
2021-04-23 13:56:42 +0200

    Ajout fichier main

---
+++ main.py
@@ -0,0 +1,13 @@
+import genius_final
+import area
+import perimeter
+
+print("Genius.py")
+print("=========")
+print("3 + 4 = " + str(genius_final.Add(4, 3)))
+print("9 - 3 = " + str(genius_final.Substract(9, 3)))
+
+print("area.py")
+print("=======")
+print("L'aire du carré de 4 cm de côté = " + str(area.SquareArea(4)))
+print("L'aire du rectangle de longueur 7 cm et 4 cm de largeur = " + str(area.RectangleArea(7,4)))
